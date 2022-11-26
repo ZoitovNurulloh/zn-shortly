0a4f9f0d443b54faeac7f303d7164d2fd6c8a4fb
ZoitovNurillo <nurillozoitov97@gmail.com>
2022-11-26 22:41:38 +0500

    Add url copy button click  handler

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
+const elUrlShortener = document.querySelector('.js-url-shortener');
 const elUrlShortenerForm = document.querySelector('.js-url-shortener__form');
 const elUrlShortnerResults = document.querySelector('.url-shortener__results');
+const elCopyUrlShortLink = document.querySelector('.js-url-shortener__copy-button');
 
-elUrlShortenerForm.addEventListener('submit' , function (evt) {
-  evt.preventDefault();
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit' , function (evt) {
+    evt.preventDefault();
 
-  elUrlShortnerResults.classList.add('url-shortener__results--open');
-})
+    elUrlShortnerResults.classList.add('url-shortener__results--open');
+  })
+};
+
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+    if (evt.target.matches('.js-url-shortener__copy-button')) {
+      evt.target.textContent = 'Copied!';
+
+      evt.target.classList.add('url-shortener__copy-button--copied');
+
+      setTimeout(function() {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('url-shortener__copy-button--copied');
+      }, 3000);
+    };
+  })
+}
