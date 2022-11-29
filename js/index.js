b7703904a715d16e9be58f7581377fa84af1f1d1
ZoitovNurillo <nurillozoitov97@gmail.com>
2022-11-29 06:56:22 +0500

    Add index-features

--- js/index.js
+++ js/index.js
@@ -14,10 +14,17 @@ if (elUrlShortenerForm) {
 if (elUrlShortener) {
   elUrlShortener.addEventListener('click', function (evt) {
     if (evt.target.matches('.js-url-shortener__copy-button')) {
+      //Change buttuon text
       evt.target.textContent = 'Copied!';
 
+      //Change button bgcolor
       evt.target.classList.add('url-shortener__copy-button--copied');
 
+      // Copy short link to clipboard
+      navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+
+
+      // Reset button text and class after 1 second
       setTimeout(function() {
         evt.target.textContent = 'Copy';
         evt.target.classList.remove('url-shortener__copy-button--copied');
